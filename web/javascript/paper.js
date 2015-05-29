e744512e3a2b5743607bf58f1e8cc8c05b4d88cb
Carl Woodward <carl@88cartell.com>
2015-05-29 19:46:02 +1000

    feat: fix size of canvas like google maps does

--- web/javascript/paper.js
+++ web/javascript/paper.js
@@ -14,11 +14,13 @@ var Paper = React.createClass({
     if (this.isDragging) {
       var dx = this.clickX - event.pageX;
       var dy = this.clickY - event.pageY;
-      this.paperX = (this.paperX || 0) - dx;
-      this.paperY = (this.paperY || 0) - dy;
+      this.paperX = (this.paperX || -5000) - dx;
+      this.paperY = (this.paperY || -3000) - dy;
+      var paper = document.querySelector('.paper');
       var matrix = 'matrix(1, 0, 0, 1, ' + this.paperX.toFixed() + ', ' +
           this.paperY.toFixed()  + ')';
-      document.querySelector('.paper').style.webkitTransform = matrix;
+      paper.style.webkitTransform = matrix;
+
       this.clickX = event.pageX;
       this.clickY = event.pageY;
     }
